805dacf40ddc6403c0d62e7d4adc5ab2c49699e1
Nikita <hrynts2002@gmail.com>
2022-04-29 17:52:50 +0300

    Third commit

---
+++ src/it/bsuir/Main.java
@@ -0,0 +1,19 @@
+package it.bsuir;
+
+import it.bsuir.memory.MemFile;
+
+public class Main {
+    public static void main(String[] inArguments) throws Throwable {
+        MemFile memFile = new MemFile();
+        System.out.print("Running... ");
+        int counter = 0;
+        long end = System.currentTimeMillis() + memFile.TIME;
+        while (System.currentTimeMillis() < end) {
+            memFile.load();
+            ++counter;
+        }
+        double count = counter / ((double) memFile.TIME / 1000);
+        System.out.println(
+                count + "/s (" + (1 / count) + ")");
+    }
+}
---
+++ src/it/bsuir/memory/MemFile.java
@@ -0,0 +1,49 @@
+package it.bsuir.memory;
+
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+public class MemFile {
+    public static final int TIME = 5000;
+    public static RandomAccessFile mFile;
+    public static ArrayList mList = new ArrayList();
+
+
+    public static void load() throws Throwable {
+        mFile = new RandomAccessFile("/proc/meminfo", "r");
+        parse();
+        mFile.close();
+    }
+
+    public static void parse() throws Throwable {
+        mFile.readLine();
+        mFile.readLine();
+        mFile.readLine();
+        mList.clear();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+        store();
+    }
+
+    public static void store() throws Throwable {
+        String line = mFile.readLine();
+        int i = 14;
+        while (line.charAt(i) == ' ') {
+            ++i;
+        }
+        int j = line.indexOf(' ', i);
+        mList.add(line.substring(i, j));
+    }
+
+}
