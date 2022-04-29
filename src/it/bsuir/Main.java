04be3068f362d41eba9a6bf235427a6d939086de
Nikita <hrynts2002@gmail.com>
2022-04-29 17:23:19 +0300

    Second commit

--- src/it/bsuir/Main.java
+++ src/it/bsuir/Main.java
@@ -4,14 +4,15 @@ import it.bsuir.memory.MemFile;
 
 public class Main {
     public static void main(String[] args) throws Throwable {
+        MemFile memFile = new MemFile();
         System.out.println("Running...");
         int counter = 0;
-        long end = System.currentTimeMillis() + MemFile.TIME;
+        long end = System.currentTimeMillis() + memFile.TIME;
         while (System.currentTimeMillis() < end) {
-            MemFile.load();
+            memFile.load();
             ++counter;
         }
-        double count = counter / ((double) MemFile.TIME / 1000);
+        double count = counter / ((double) memFile.TIME / 1000);
         System.out.println(count + "/s (" + (1 / count) + ")");
     }
 }
