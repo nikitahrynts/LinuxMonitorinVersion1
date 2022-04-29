418c8cc858c371e2635a032f83b25e0e45426eb8
Nikita <hrynts2002@gmail.com>
2022-04-29 17:35:39 +0300

    Third commit

--- src/it/bsuir/Main.java
+++ src/it/bsuir/Main.java
@@ -1,6 +1,7 @@
 package it.bsuir;
 
-import it.bsuir.memory.MemFile;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Throwable {
@@ -14,5 +15,52 @@ public class Main {
         }
         double count = counter / ((double) memFile.TIME / 1000);
         System.out.println(count + "/s (" + (1 / count) + ")");
+    }
+
+    public static class MemFile {
+        public static final int TIME = 5000;
+
+        public static RandomAccessFile randomAccessFile;
+
+        public static ArrayList arrayList = new ArrayList<>();
+
+        public static void load() throws Throwable {
+            randomAccessFile = new RandomAccessFile("/proc/meminfo", "r");
+            parse();
+            randomAccessFile.close();
+        }
+
+        private static void parse() throws Throwable {
+            randomAccessFile.readLine();
+            randomAccessFile.readLine();
+            randomAccessFile.readLine();
+            arrayList.clear();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+            store();
+        }
+
+        private static void store() throws Throwable {
+            String line = randomAccessFile.readLine();
+            int i = 14;
+            while (line.charAt(i) == ' ') {
+                ++i;
+            }
+            int j = line.indexOf(' ', i);
+            arrayList.add(line.substring(i, j));
+        }
+
+
     }
 }
