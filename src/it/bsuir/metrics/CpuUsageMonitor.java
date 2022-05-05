ef0d54d07cb150649eae9054ba972f6505f2b5e2
Nikita <hrynts2002@gmail.com>
2022-05-06 02:47:19 +0300

    Add PerformanceMonitor

--- src/it/bsuir/metrics/CpuUsageMonitor.java
+++ src/it/bsuir/metrics/CpuUsageMonitor.java
@@ -1,20 +1,54 @@
 package it.bsuir.metrics;
 
-import com.sun.management.OperatingSystemMXBean;
-
-import java.lang.management.ManagementFactory;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CpuUsageMonitor {
 
-    public static void printUsage() {
-        OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) ManagementFactory
-                .getOperatingSystemMXBean();
+    public static double printUsage() throws InterruptedException{
+        try {
 
-        while (true) {
-            System.out.println(bean.getProcessCpuLoad());
-            System.out.println(bean.getSystemCpuLoad());
+            long delay = 50;
+            List<Double> listValues = new ArrayList<Double>();
+            for (int i = 0; i < 100; i++) {
+                long cput1 = getCpuT();
+                Thread.sleep(delay);
+                long cput2 = getCpuT();
+                double cpuproc = (1000d * (cput2 - cput1)) / (double) delay;
+                listValues.add(cpuproc);
+            }
+            listValues.remove(0);
+            listValues.remove(listValues.size() - 1);
+            double sum = 0.0;
+            for (Double double1 : listValues) {
+                sum += double1;
+            }
+            return sum / listValues.size();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
         }
+
+    }
+
+    private static long getCpuT() throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("/proc/stat"));
+        String line = reader.readLine();
+        Pattern pattern = Pattern.compile("\\D+(\\d+)\\D+(\\d+)\\D+(\\d+)\\D+(\\d+)");
+        Matcher m = pattern.matcher(line);
+
+        long cpuUser = 0;
+        long cpuSystem = 0;
+        if (m.find()) {
+            cpuUser = Long.parseLong(m.group(1));
+            cpuSystem = Long.parseLong(m.group(3));
+        }
+        return cpuUser + cpuSystem;
     }
 }
